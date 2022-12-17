d5d3049553721bdc5daa64bb5982e851ff33e45a
MAC <my@MACdeAir.lan>
2022-12-17 19:37:28 +0800

    add empty dashboard and sizebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import Topbar from './scenes/global/Topbar';
+import Dashboard from './scenes/dashboard'
+import Sidebar from './scenes/global/Sidebar'
+import { Routes, Route } from 'react-router-dom'
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -9,8 +12,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline></CssBaseline>
         <div className="app">
+          <Sidebar />
           <main className='content'>
             <Topbar></Topbar>
+            <Routes>
+              <Route path="/" element={<Dashboard />}></Route>
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
