5db287a38db912c91b08f14fa7cb65f95c6e5d63
MAC <my@MACdeAir.lan>
2022-12-20 18:42:14 +0800

    add admin image to sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -65,6 +65,29 @@ const QiangZhouSidebar = () => {
                             </Box>
                         )}
                     </MenuItem>
+
+                    {
+                        !isCollapsed && (
+                            <Box mb="25px">
+                                <Box display="flex" justifyContent="center" alignItems="center">
+                                    <img alt="profile-user"
+                                        widtd="100px"
+                                        height="100px"
+                                        src={'../../assets/user.png'}
+                                        style={{ cursor: "pointer", borderRadius: "50%" }}
+                                    />
+
+                                </Box>
+                                <Box textAlign="center">
+                                    <Typography
+                                        variant="h2"
+                                        color={colors.grey[100]}
+                                        fontWeight="bold" sx={{ m: "10px 0 0 0" }}>Ed Roh</Typography>
+                                    <Typography variant="h5" color={colors.greenAccent[500]}>VP Admin</Typography>
+                                </Box>
+                            </Box>
+                        )
+                    }
                 </Menu>
             </ProSidebar>
         </Box>
